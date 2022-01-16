131a3953954910d623c4902347d6eaa3400c2c23
Ankit Maurya <ankitmaurya18@cse.iiitp.ac.in>
2022-01-16 17:07:33 +0530

    updated code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,15 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
 import './src/styles/global.css'
+
+export const Global = ({ element, props }) => {
+    return <React.Fragment>
+        <Helmet>
+            <title>Ankit Maurya</title>
+            <meta name="description" content="Home page" />
+            <meta charset="UTF-8" />
+            <meta name="google-site-verification" content="MysWIXF-YqnH7cUNS1vp5z5Jxot0Ho0CS7NMFhg7s3Q" />
+        </Helmet>
+        {element}
+    </React.Fragment>
+}
