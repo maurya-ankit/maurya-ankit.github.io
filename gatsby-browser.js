59aa783e017932b3b485d1e7c344bf6ee22c67cd
Ankit Maurya <ankitmaurya18@cse.iiitp.ac.in>
2022-01-16 17:32:39 +0530

    seo implementation

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
-import './src/styles/global.css'
+require('./src/styles/global.css');
+const { Helmet } = require('react-helmet');
+const React = require("react");
 
-export const Global = ({ element, props }) => {
+exports.wrapRootElement = ({ element, props }) => {
     return <React.Fragment>
         <Helmet>
             <title>Ankit Maurya</title>
             <meta name="description" content="Home page" />
             <meta charset="UTF-8" />
+
             <meta name="google-site-verification" content="MysWIXF-YqnH7cUNS1vp5z5Jxot0Ho0CS7NMFhg7s3Q" />
         </Helmet>
         {element}
