de933a1fc06b25d0b0decb0ff3db74f165fe1f85
Ankit Maurya <ankitmaurya18@cse.iiitp.ac.in>
2022-01-16 15:41:42 +0530

    workflow commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,39 @@
 module.exports = {
-    siteMetadata: {
-      title: `maurya-ankit.github.io`,
-        siteUrl: `https://www.yourdomain.tld`,
+  siteMetadata: {
+    title: `maurya-ankit.github.io`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    'gatsby-plugin-postcss',
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Defaults used for gatsbyImageData and StaticImage
+        defaults: {},
+        // Set to false to allow builds to continue on image errors
+        failOnError: true,
+        // deprecated options and their defaults:
+        base64Width: 20,
+        forceBase64Format: `webp`, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+      },
     },
-    plugins: [
-
-    ]
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Ankit Maurya`,
+        short_name: `Ankit Maurya`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -129,6 +129,9 @@ const links = [
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <title>Home Page</title>
       <h1 style={headingStyles}>
         Congratulations
