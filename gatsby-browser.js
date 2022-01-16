dfeda26e003aad4e1b69e00d21fa5e80e90a213a
Ankit Maurya <ankitmaurya18@cse.iiitp.ac.in>
2022-01-16 21:25:46 +0530

    minor bug fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,1 @@
 require('./src/styles/global.css');
-const { Helmet } = require('react-helmet');
-const React = require("react");
-
-exports.wrapRootElement = ({ element, props }) => {
-    return <React.Fragment>
-        <Helmet>
-            <title>Ankit Maurya</title>
-            <meta name="description" content="Home page" />
-            <meta charset="UTF-8" />
-            <meta name="google-site-verification" content="MysWIXF-YqnH7cUNS1vp5z5Jxot0Ho0CS7NMFhg7s3Q" />
-        </Helmet>
-        {element}
-    </React.Fragment>
-}
--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,8 @@ module.exports = {
     siteUrl: `https://maurya-ankit.github.io`,
     description: `Ankit Maurya's personal website.`,
     author: `Ankit Maurya`,
-    keywords: [`ankit maurya`, `portfolio`]
+    keywords: [`ankit maurya`, `portfolio`],
+    'google-site-verification': `MysWIXF-YqnH7cUNS1vp5z5Jxot0Ho0CS7NMFhg7s3Q`
   },
   plugins: [
     'gatsby-plugin-postcss',
